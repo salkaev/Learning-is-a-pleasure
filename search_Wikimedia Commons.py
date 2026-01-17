62a3e277cc75b2df286f5d5c42359c232afa4228
salkaevi <salkaevi@gmail.com>
2026-01-17 17:13:53 +0300

    wiki requist 0.1

--- search_Wikimedia Commons.py
+++ search_Wikimedia Commons.py
@@ -1,6 +1,6 @@
 import requests
 
-def search_commons(query, limit=1):
+def search_commons(query, limit=10):
     # 1. Базовый URL API
     url = "https://commons.wikimedia.org/w/api.php"
 
@@ -9,16 +9,36 @@ def search_commons(query, limit=1):
         "action": "query",  
         "format": "json",
         "list": "search",
-        "srsearch": query,
         "srnamespace": 6,
-        "srlimit": limit         
+        "srlimit": limit ,
+        "srlanguage": "en",
+        "srsearch": query
     }
 
     response = requests.get(url, params=params, 
                           headers={"User-Agent": "CourseworkBot/1.0"})
-    
     return response.json()
+##
+result = search_commons("C++ compilers",)
+print (result)
+# 1. Извлекаем название файла
+first_result = result["query"]["search"][0]
+filename = first_result["title"]  # "File:Compiling in C-ar.svg"
+clean_filename = filename.replace("File:", "")  # "Compiling in C-ar.svg"
 
-result = search_commons("software diagram", limit=1)
-    
-print(result)
+print(f"Название файла: {clean_filename}")
+
+# 2. Формируем URL для скачивания
+download_url = f"https://commons.wikimedia.org/wiki/Special:FilePath/{clean_filename}"
+print(f"🔗 URL для скачивания: {download_url}")
+
+# 3. Скачиваем файл
+headers = {"User-Agent": "CourseworkBot/1.0"}
+response = requests.get(download_url, headers=headers)
+
+# 4. Сохраняем
+save_as = "compiling_diagram.svg"
+with open(save_as, "wb") as f:
+    f.write(response.content)
+
+#print(f"Файл сохранён как: {save_as} ({len(response.content)} байт)")
