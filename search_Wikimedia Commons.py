5393ae1639bd15b5a8443817a1bee7f39ed49b5b
salkaevi <salkaevi@gmail.com>
2026-02-21 11:08:25 +0300

    Add new logic for saving files

--- search_Wikimedia Commons.py
+++ search_Wikimedia Commons.py
@@ -1,8 +1,8 @@
 import requests
 import sys
 
-#query = sys.argv[1] # Данные с названием из основной части кода
-query = "Bayraktar TB2 photo"
+cnt = 0 
+query = sys.argv[1] # Данные с названием из основной части кода
 
 def search_commons(query, limit=1):
     url = "https://commons.wikimedia.org/w/api.php"
@@ -33,14 +33,15 @@ print(f"Название файла: {clean_filename}")
 
 # 2. Формируем URL для скачивания
 download_url = f"https://commons.wikimedia.org/wiki/Special:FilePath/{clean_filename}"
-print(f"🔗 URL для скачивания: {download_url}")
+print(f"URL для скачивания: {download_url}")
 
 # 3. Скачиваем файл
 headers = {"User-Agent": "CourseworkBot/1.0"}
 response = requests.get(download_url, headers=headers)
 
 # 4. Сохраняем
-save_as = "Bayraktar TB2.jpg"
+cnt += 1
+save_as = f"{query} {cnt}"
 with open(save_as, "wb") as f:
     f.write(response.content)
 
